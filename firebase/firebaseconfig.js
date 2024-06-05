fd801bd8167e85ef0b159acf238afe3dbda0ab97
Duongvanhung2003 <chomotlat1234@gmail.com>
2024-06-05 10:21:17 +0700

    first

--- firebase/firebaseconfig.js
+++ firebase/firebaseconfig.js
@@ -20,5 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const authentication =getAuth(app);
